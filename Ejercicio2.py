8d782babba63af6475811ed1cbf6d3cb340e05a7
ElenaCervera <elena.cerveralopez@usp.ceu.es>
2022-11-06 18:06:47 +0100

    commit 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,2 +1,33 @@
 #Realiza el  código para calcular el determinante de una matriz cuadrada de [3 x 3], regla de Sarrus de forma recursiva y de forma iterativa
+matriz=[]
+def creamatriz(n):
+    for i in range(n):
+        matriz.append([])
+        for j in range(n):
+            matriz[i].append(0)
+    return matriz
+
+def multiplicar(n):
+    n1= int(n[0][0]*n[1][1]*n[2][2])
+    n2= int(n[1][0]*n[2][1]*n[0][2])
+    n3= int(n[2][0]*n[0][1]*n[1][2])   
+    ntotal=n1+n2+n3
+    return ntotal
+
+def invers(n):
+    inversa=[]
+    for i in n:
+        t=i[::-1]
+        inversa.append(t)
+    return inversa
+
+def det(n):
+    matriz=creamatriz(n)
+    matinvers=invers(matriz)
+    determinante=multiplicar(matriz) - multiplicar(matinvers)     
+    return determinante
+
+n=[3,5,8]
+det(n)    
+
 
--- Ejercicio4.py
+++ Ejercicio4.py
@@ -57,7 +57,60 @@ class Polinomio (object):
                 aux=aux.sig
         return pol
 
-        
-                                 
+
+    def restar(polinomio1, polinomio2):
+        paux=Polinomio()
+        mayor=polinomio1 if (polinomio1 > polinomio2) else polinomio2
+        for i in range (0, mayor.grado + 1):
+            total= obtener_valor(polinomio2, i) - obtener_valor(polinomio1, i)
+            if total != 0:
+                agregar_termino(paux, i, total)
+        return paux
+
+    def dividir(polinomio1, polinomio2):
+        paux=Polinomio()
+        pol1=polinomio1.termino_mayor
+        while (pol1 is not None):
+            pol2 =polinomio2.termino_mayor
+            while(pol2 is not None):
+                termino=pol1.info.termino + pol2.info.termino  
+                valor=pol1.info.valor / pol2.info.valor
+                if(obtener_valor(paux, termino)!=0):
+                   valor += obtener_valor(paux, termino)
+                   modificar_termino(paux, termino, valor)
+                else:
+                    agregar_termino(paux, termino, valor)
+
+            pol2=pol2.sig
+            pol1=pol1.sig
+        return paux
+
+        def eliminar_termino(polinomio, clave):
+            dato=None
+            if(polinomio.inicio.info==clave):
+                dato - polinomio.inicio.info
+                polinomio.inicio =polinomio.inicio.sig
+                polinomio.tamano -=1
+            else:
+                anterior =polinomio.inicio
+                actual=polinomio.inicio.sig
+                while(actual is not None and actual.info !=clave):
+                    anterior=anterior.sig
+                    actual=actual.sig
+                if (actual is not None):
+                    dato=actual.info
+                    anterior.sig=actual.sig
+                    polinomio.tamano-=1
+            return dato            
+
+
+
+
+
+
+
+
+
+
 
 
