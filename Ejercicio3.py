7e6fe50a7e1f34019353b80c751f383253f1828c
ElenaCervera <elena.cerveralopez@usp.ceu.es>
2022-11-06 18:43:55 +0100

    commit 5

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -9,3 +9,25 @@
 #mostrar todas las naves que comienzan con AT;
 #listar todas las naves que pueden llevar seis o más pasajeros;
 # mostrar toda la información de la nave más pequeña y la más grande
+
+
+class Nave:
+    def __init__(self, nombre, largo, tripulacion, pasajeros):
+        self.nombre=nombre
+        self.largo=largo
+        self.tripulacion=tripulacion
+        self.pasajeros=pasajeros
+    def __str__(self):
+        return "nombre{}, {} largo, {} tripulacion, {} pasajeros".format(self.nombre, self.largo, self.tripulacion, self.pasajeros)
+
+nave1=Nave("Halcón Milenario",200, 20, 75)
+nave2=Nave("Estrella de la muerte", 150, 15, 60)
+nave3=Nave("Luna llena", 100, 30, 200)
+nave4= Nave("ATestrellas fugaces", 250, 80, 400)
+nave5=Nave("PEqueñanave", 50, 2, 5)
+nave6=Nave("Starwars", 50, 8, 20)
+
+
+
+
+
